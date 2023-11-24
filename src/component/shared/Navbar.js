cef1246c662fb4e1767d7aa617558264faa2da5e
shafayat zamil <zamilkhan111.za@gmail.com>
2023-11-24 10:50:02 +0600

    little changed

--- src/component/shared/Navbar.js
+++ src/component/shared/Navbar.js
@@ -37,91 +37,8 @@ const Navbar = () => {
   };
 
   return (
-    // <div>
-    //   <nav className="flex flex-col md:flex-col lg:flex-row md:items-center  gap-10 items-center justify-between p-4 bg-violet-100">
-    //     {/* navbar link */}
-    //     <div className="flex flex-col md:flex-col lg:flex-row  justify-center items-center  gap-4 mx-8">
-    //       <div className="flex  gap-32 justify-around">
-    //         <Link to="/">
-    //           <div className="">
-    //             <FontAwesomeIcon
-    //               icon={faEnvelope}
-    //               beatFade
-    //               size="lg"
-    //               style={{ color: "#4f6da1" }}
-    //             />
-    //             <span className="font-bold text-xl tracking-tight text-primary-light font-kumbh-sans cursor-pointer mr-4">
-    //               Estatery
-    //             </span>
-    //           </div>
-    //         </Link>
-
-    //         <span
-    //           onClick={toggleNav}
-    //           className="justify-self-end md:hidden lg:hidden"
-    //         >
-    //           <FontAwesomeIcon icon={nav === true ? faXmark : faBars} />
-    //         </span>
-    //       </div>
-
-    //       <div className="hidden md:flex space-x-4">
-    //         <ul className="flex lg:flex-row md:flex-col flex-col  gap-6 justify-center items-center text-base ">
-    //           {links.map(({ to, name }) => (
-    //             <Link to={to} key={name} className="nav-link">
-    //               {name}
-    //             </Link>
-    //           ))}
-    //         </ul>
-    //       </div>
-    //     </div>
-    //     {/* navbar button */}
-
-    //     <div className="text-base mx-8 hidden md:flex space-x-4">
-    //       {user ? (
-    //         <>
-    //           <div className="flex gap-2  ">
-    //             <button
-    //               className="bg-[#7065F0] text-white font-medium  px-3 py-1 rounded-md"
-    //               onClick={handleLogOut}
-    //             >
-    //               {" "}
-    //               Logout
-    //             </button>
-    //             <div>
-    //               <img
-    //                 src={user.photoURL ? user.photoURL : LoginUserImage}
-    //                 className="w-6 h-8"
-    //                 alt="user"
-    //               />
-    //             </div>
-    //           </div>
-    //         </>
-    //       ) : (
-    //         <>
-    //           <Link to="/login">
-    //             {" "}
-    //             <button className="border border-black py-1 font-medium px-3 rounded-md mr-3">
-    //               Login
-    //             </button>
-    //           </Link>
-    //           <Link to="/register">
-    //             <button className="bg-[#7065F0] text-white font-medium  px-3 py-1 rounded-md">
-    //               Signup
-    //             </button>
-    //           </Link>
-    //         </>
-    //       )}
-    //     </div>
-
-    //     {/* responsive navbar button */}
-
-    //         </div>
-    //       </>
-    //     )}
-    //   </nav>
-    // </div>
     <nav className=" relative bg-gradient-to-r from-violet-100 to-violet-200">
-      <div className="flex items-center justify-between py-4 text-gray-700 text-md mx-8">
+      <div className="flex items-center justify-between py-4 text-gray-700  text-md mx-8">
         <Link to="/">
           <div className="flex items-center justify-between gap-1">
             <FontAwesomeIcon
@@ -130,7 +47,7 @@ const Navbar = () => {
               size="lg"
               style={{ color: "#4f6da1" }}
             />
-            <span className="font-bold text-xl tracking-tight text-primary-light font-kumbh-sans cursor-pointer mr-6">
+            <span className="font-bold text-2xl text-[#1d1160] tracking-tight text-primary-light font-kumbh-sans cursor-pointer mr-6">
               Estatery
             </span>
           </div>
@@ -146,7 +63,7 @@ const Navbar = () => {
         <div className="hidden md:flex">
           <ul className="flex gap-6">
             {links.map(({ to, name }) => (
-              <Link to={to} key={name} className="nav-link">
+              <Link to={to} key={name} className="nav-link text-black text-md">
                 {name}
               </Link>
             ))}
